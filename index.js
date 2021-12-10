eff7a175668986ded5665246b876fb01fa234b74
Giyanaryoga Puguh SP <giyanaryoga@gmail.com>
2021-12-10 20:54:10 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
-
 const app = express();
+const router = require("./routes/index.js");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
